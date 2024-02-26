b9fb5b1a95b0166ba08760c97b2d931f8c019041
JefG67 <jefguilpain@gmail.com>
2024-02-26 16:12:21 +0100

    mettre un clic pour inverser icone

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// On récupère l'élément #board
+const board = document.querySelector("#board");
+
+// On crée une box avec la class "carree"
+const box = document.createElement("div")
+box.classList.add("box")
+
+
+
+const icones = ["fa-facebook", "fa-twitter", "fa-instagram"];
+const couleurs = ["#3b5998", "#1D9BF0", "#E1306C"];
+
+for (let i = 0; i <= 2; i++) {
+   const newBox = box.cloneNode(); // Clone des divs
+   newBox.style.backgroundColor = couleurs[i]
+   const newIcone = document.createElement("i")
+   newIcone.classList.add("fa-brands")
+   newIcone.classList.add(icones[i])
+   newBox.appendChild(newIcone)
+   board.appendChild(newBox)
+}
+
+
+
+let boxClick = document.querySelectorAll(".box")
+boxClick.forEach(function(box){
+    box.addEventListener("click",function(){
+    box.classList.toggle("click")
+    })
+}
+)
+let boxUnClick = document.querySelectorAll(".box")
+boxUnClick.forEach(function(box){
+    box.addEventListener("unClick",function(){
+    box.classList.toggle("unClick")
+    })
+}
+)
